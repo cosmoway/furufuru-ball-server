0681e01275b52c7a27a95f554d506efea1385f47
Yuuki Shimizu <shimizu@cosmoway.net>
2015-07-16 00:28:15 +0900

    Websocket コネクションを配列で管理

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 /**
  * furufuru-ball-server
  *
- * @author: yuukis (Cosmoway factory4)
+ * @author: Yuuki Shimizu at Cosmoway
  */
 var WebSocketServer = require('ws').Server
     , http = require('http')
@@ -12,8 +12,24 @@ app.use(express.static(__dirname + '/'));
 var server = http.createServer(app);
 var wss = new WebSocketServer({server: server});
 
+// Websocket コネクション配列
+var connections = [];
+
 // 接続時
 wss.on('connection', function connection(ws) {
+
+    // WebSocket コネクションを配列に保存
+    connections.push(ws);
+
+    // 切断時
+    ws.on('close', function () {
+        // WebSocket コネクションを配列から削除
+        connections = connections.filter(function (conn, i) {
+            return (conn !== ws);
+        });
+    });
+
+    // メッセージ受信時
     ws.on('message', function incoming(message) {
         console.log('received: %s', message);
     });
